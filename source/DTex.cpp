c63497b0303acc23142b2b1b770b4a1c353f8564
zz <zhuguang@ejoy.com>
2019-01-07 06:20:11 +0800

    add log

--- source/DTex.cpp
+++ source/DTex.cpp
@@ -120,6 +120,7 @@ draw_begin()
 		std::static_pointer_cast<pt2::Shader>(shader)->UpdateProjMat(2, 2);
 		std::static_pointer_cast<pt2::Shader>(shader)->UpdateViewMat(sm::vec2(0, 0), 1);
 		//shader->SetMat4("u_model", sm::mat4().x);
+//        shader->UpdateModelMat(sm::mat4().x);
 	}
 }
 
